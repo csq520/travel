b45647a04c64960d26be527decbd68df129561fd
csq <2568333290@qq.com>
2019-07-30 00:24:27 +0800

    change
    Signed-off-by: csq <2568333290@qq.com>

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,16 +1,20 @@
 <template>
     <div>
       <div class="home"><HomeHeader></HomeHeader></div>
+      <div class="home"><HomeSwiper></HomeSwiper></div>
       <!--<router-link to="/list">列表页</router-link>-->
     </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
+
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="" />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/818f6cc784ae6669b74bbbb255414a53.jpg_750x200_66ca5873.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/a5d26ebec399b3aef013f45771ae96f0.jpg_750x200_de947eef.jpg'
+      }, {
+        id: '0003',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/55991e51bf47026268885d8161c5cc97.png_750x200_75e2b2c9.png'
+      }, {
+        id: '0004',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/ae8987bff39ff10e82675a3643154e66.jpg_750x200_0f187b2e.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff;
+  .wrapper
+    width: 100%;
+    height: 0;
+    padding-bottom 31.25%
+    overflow hidden
+    .swiper-img
+      width: 100%;
+</style>
