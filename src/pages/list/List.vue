0b4e18bcaccba45694533410b1a6b01b431d9d3c
csq <2568333290@qq.com>
2019-07-29 21:11:11 +0800

    项目初健
    Signed-off-by: csq <2568333290@qq.com>

---
+++ src/pages/list/List.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="list">list</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List'
+}
+</script>
+
+<style scoped>
+  .list {
+    font-size: 50px;
+  }
+</style>
